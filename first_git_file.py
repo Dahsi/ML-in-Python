b3824fef1089a7d99cca93e6c6fb49646dd09492
Dahsi <alanchc@gmail.com>
2015-10-10 10:44:27 +0800

    Add first_git_file.py file to learn git function.

---
+++ first_git_file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 10:37:56 2015
+
+@author: alanchc
+"""
+
+def main():
+    print "This is my first .py commited to Github!"
+
+if __name__ == '__main__':
+    main()
